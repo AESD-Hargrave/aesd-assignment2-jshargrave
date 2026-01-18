d378c2145cf4ea94031583f71e0680e9b31d59a2
joe <gitea@itshappy.org>
2026-01-18 14:27:04 -0700

    Finished writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -20,9 +20,74 @@
 */
 
 #include "stdio.h"
+#include <syslog.h>
+#include "string.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int main()
+// File flags: write only and create file if it does not exist
+#define FILE_FLAGS O_WRONLY | O_CREAT
+// File mode: User has read/write, groupd has read/write, everyone has read permissions
+#define FILE_MODE S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH
+
+int main(int argc, char *argv[])
 {
-    printf("Hello World!\n");
+    // Setup logger
+    openlog(NULL, LOG_ODELAY, LOG_USER);
+
+    // If missing writefile
+    if (argc < 2)
+    {
+        syslog(LOG_ERR, "%s", "Error: Missing writefile argument!");
+        return 1;
+    }
+
+    // If missing writestr
+    if (argc < 3)
+    {
+        syslog(LOG_ERR, "%s", "Error: Missing writestr argument!");
+        return 1;
+    }
+
+    // Create and open file
+    int file_descriptor = open(argv[1], FILE_FLAGS, FILE_MODE);
+    
+    // Check that file opened correctly
+    if (file_descriptor == -1)
+    {
+        syslog(LOG_ERR, "Error: Failed to open file '%s'!", argv[1]);
+        return 1;
+    }
+
+    // Log write
+    syslog(LOG_DEBUG, "Writing '%s' to '%s'", argv[2], argv[1]);
+
+    // Write contents to file
+    size_t write_length = strlen(argv[1]);
+    ssize_t bytes_written = write(file_descriptor, argv[1], write_length);
+    if (bytes_written == -1)
+    {
+        syslog(LOG_ERR, "Error: Failed to write to file '%s'!", argv[1]);
+        return 1;
+    }
+    else if (bytes_written != write_length)
+    {
+        syslog(LOG_ERR, "Error: Failed to write all content to file '%s'!", argv[1]);
+        return 1;
+    }
+
+    // Close file
+    int close_return = close(file_descriptor);
+    if (close_return == -1)
+    {
+        syslog(LOG_ERR, "Error: Failed to close file '%s'!", argv[1]);
+        return 1;
+    }
+    
+    // Close log
+    closelog();
+    
     return 0;
 }
